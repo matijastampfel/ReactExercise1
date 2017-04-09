ba4001d9c022a30c8d32bf2e85cb3f2d054a9a2f
matijastampfel <matija.stampfel@gmail.com>
2017-04-09 18:15:30 +0200

    v0.2.1

--- script.jsx
+++ script.jsx
@@ -1,2 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+var React = require('react');
+var ReactDOM = require ("react-dom");
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <p>
+                Hi
+            </p>
+        );
+    }
+}
+
+export default componentName;
