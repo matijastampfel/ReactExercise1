9da0056e9a3c327d41da98a9c6dbcd5e2b927ad0
matijastampfel <matija.stampfel@gmail.com>
2017-04-11 20:54:42 +0200

    final

--- script.jsx
+++ script.jsx
@@ -1,50 +1,120 @@
-var React = require('react');
-var ReactDOM = require ("react-dom");
+/*jshint esnext: true, moz: true*/
+/*jslint browser:true */
+/*global React.Component, React, ReactDOM */
+
+// first part/////////////////////////////////////////////////////////////////////////////
+
+
+var app = document.getElementById("app");
 
 
 
-class App extends React.Component {
+
+class TextEdit extends React.Component{
+
     constructor(props){
         super(props);
-        this.state= {
-            message1: "",
-            message2: ""
-        };
-        this.handleKeyUp1 = this.handleKeyUp1.blind(this);
-        this.handleKeyUp2 = this.handleKeyUp2.blind(this);
-
+        this.state = {};
+        this.setValue = this.setValue.bind(this);
     }
 
-    handleKeyUp1(event) {
-        this.setState({
-            message2: event.target.value
-        });
+    setValue(e){
+        var value = e.target.value;
+        this.setState({valueX:value});
+       
+    
     }
-handleKeyUp2(event){
-    this.setState({
-        message1: event.target.value
-    });
-}
 
 
     render() {
-        return (
-            <div className="myform" >
-            <MyForm handleKeyUp = {this.handleKeyUp1} message = {this.state.message1} />
-            <MyForm handleKeyUp = {this.handleKeyUp2} message = {this.state.message2} />
-           </div>
+       
+        return(
+            <section>
+            <input onChange={this.setValue} type="text"/>
+            <p >{this.state.valueX}</p>
+            </section>
         );
     }
 }
 
-function MyForm(props) {
-    return <div>
-        <input type="text" onKeyUp= {props.handleKeyUp} />
-        <span>{props.message}</span>
-        </div>
+ReactDOM.render(<TextEdit/>, app);
+
+//***************************************************************************************************** */
+
+class SumText extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {value1:0, value2:0};
+        this.sumValue1 = this.sumValue1.bind(this);
+        this.sumValue2 = this.sumValue2.bind(this);
+    }
+
+sumValue1(e){
+    var value1 = e.target.value;
+    this.setState({value1:value1})
 }
 
-ReactDOM.render(<App></App>,
-document.getElementById("app")
+sumValue2(e){
+    var value2 = e.target.value;
+    this.setState({value2:value2})
+}
 
-)
+    render() {
+       let sum = Number(this.state.value1)+Number(this.state.value2);
+        return(
+            <section>
+            <input onChange = {this.sumValue1}  type="text"/>
+           
+            <input onChange = {this.sumValue2}  type="text"/>
+
+            <p>{sum}</p>
+            </section>
+        );
+        
+    }
+
+}
+
+ReactDOM.render(<SumText/>, hola);
+// **************************************************************************************//
+
+//Button state//////////////////////////////////////////////////////////////////////////////
+
+class Button extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+        this.button = this.button.bind(this);
+    }
+
+button(e){
+    var bt = document.getElementsByClassName("btns");
+    for(var i = 0; i < bt.length; i++){
+        bt[i].className = "btns";
+    }
+    var button = e.target;
+    this.setState({title:button.title})
+    button.className = "clicked btns";
+    
+
+}
+
+render() {
+    return(
+<section>
+    <button onClick={this.button} title="First button" className="btns">First button</button>
+    <button onClick={this.button} title="Second button" className="btns">Second button</button>
+    <p>
+        {this.state.title}
+    </p>
+</section>
+    );
+}
+
+
+}
+
+ReactDOM.render(<Button/>, document.getElementById("gumb"))
+//************************************************************************************** */
